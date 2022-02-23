742d7a74ae75d242bab5e70ee79e8e99fcb7f2fc
JHB <jhernandezb96@gmail.com>
2022-02-23 18:17:31 -0500

    key_value_app_exporter: +core module

--- lib/src/repo/objectbox/key_value_repo/app/key_value_app_exporter.dart
+++ lib/src/repo/objectbox/key_value_repo/app/key_value_app_exporter.dart
@@ -1,5 +1,6 @@
 library key_value_app_exporter;
 
+export 'a_module/key_value_core_module.dart';
 export 'b_domain/key_value_domain_exporter.dart';
 export 'c_usecase/key_value_use_case_exporter.dart';
 export 'd_usecase_impl/key_value_usecase_impl_exporter.dart';
