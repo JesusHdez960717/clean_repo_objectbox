92765f21a0e592c9b9a1e9ebafcfbfa8b3f2806b
JHB <jhernandezb96@gmail.com>
2022-02-23 12:40:34 -0500

    KeyValueEntity: rename

--- lib/src/repo/objectbox/key_value_repo/repo/b_entity/entities.dart
+++ lib/src/repo/objectbox/key_value_repo/repo/b_entity/entities.dart
@@ -2,7 +2,7 @@ import 'package:clean_core/clean_core.dart';
 import 'package:objectbox/objectbox.dart';
 
 @Entity() // flutter pub run build_runner build
-class ParentEntity extends BasicEntityObject {
+class KeyValueEntity extends BasicEntityObject {
   @Id()
   int id;
 
@@ -10,7 +10,7 @@ class ParentEntity extends BasicEntityObject {
 
   String value;
 
-  ParentEntity(this.key, this.value,
+  KeyValueEntity(this.key, this.value,
       {this.id = 0}); //default construct, DON'T REMOVE
 
 }
