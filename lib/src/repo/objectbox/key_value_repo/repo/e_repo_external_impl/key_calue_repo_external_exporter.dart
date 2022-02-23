a8cae22c802639bf848ea09d5131d0c2f0961dcb
JHB <jhernandezb96@gmail.com>
2022-02-23 12:49:59 -0500

    +repo/repo_external_impl

---
+++ lib/src/repo/objectbox/key_value_repo/repo/e_repo_external_impl/key_calue_repo_external_exporter.dart
@@ -0,0 +1,4 @@
+library key_value_repo_external_exporter;
+
+//exporta los repos externos
+export 'key_value_repo_external_impl.dart';
--- lib/src/repo/objectbox/key_value_repo/repo/key_value_repo_exporter.dart
+++ lib/src/repo/objectbox/key_value_repo/repo/key_value_repo_exporter.dart
@@ -3,3 +3,4 @@ library key_value_repo_exporter;
 export 'b_entity/entities.dart';
 export 'c_repo_impl/key_value_repo_impl_exporter.dart';
 export 'd_repo_external/key_value_repo_external_exporter.dart';
+export 'e_repo_external_impl/key_calue_repo_external_exporter.dart';
