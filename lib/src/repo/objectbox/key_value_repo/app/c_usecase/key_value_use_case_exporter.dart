4965583d9a4ec12a0471b822b06c386871e5c2a9
JHB <jhernandezb96@gmail.com>
2022-02-23 12:10:27 -0500

    +app/use_case

---
+++ lib/src/repo/objectbox/key_value_repo/app/c_usecase/key_value_use_case_exporter.dart
@@ -0,0 +1,4 @@
+library key_value_usecase_exporter;
+
+//exporta los usecase def
+export 'key_value_use_case.dart';
--- lib/src/repo/objectbox/key_value_repo/app/key_value_app_exporter.dart
+++ lib/src/repo/objectbox/key_value_repo/app/key_value_app_exporter.dart
@@ -1,1 +1,3 @@
 library key_value_app_exporter;
+
+export 'b_domain/key_value_domain_exporter.dart';
