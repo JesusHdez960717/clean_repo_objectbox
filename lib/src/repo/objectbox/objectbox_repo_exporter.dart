1bce0282a4c2a66d8dff93154faec41dca2bb2e3
JHB <jhernandezb96@gmail.com>
2022-02-23 18:11:01 -0500

    objectbox_repo_exporter: +key_value_exporter

--- lib/src/repo/objectbox/objectbox_repo_exporter.dart
+++ lib/src/repo/objectbox/objectbox_repo_exporter.dart
@@ -1,4 +1,5 @@
 library objectbox_repo_exporter;
 
 export 'default_objectbox_crud_repository_external.dart';
+export 'key_value_repo/key_value_exporter.dart';
 export 'objectbox_crud_repository_external.dart';
