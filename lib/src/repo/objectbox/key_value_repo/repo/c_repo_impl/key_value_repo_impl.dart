600178afd3e94badf33512f525648b4bfce97a36
JHB <jhernandezb96@gmail.com>
2022-02-23 16:32:17 -0500

    KeyValueRepoImpl: modify for all in KeyValueDomain

--- lib/src/repo/objectbox/key_value_repo/repo/c_repo_impl/key_value_repo_impl.dart
+++ lib/src/repo/objectbox/key_value_repo/repo/c_repo_impl/key_value_repo_impl.dart
@@ -3,35 +3,31 @@ import 'package:clean_repo_objectbox/src/repo/objectbox/key_value_repo/key_value
 
 class KeyValueRepoImpl extends DefaultCRUDRepo<KeyValueDomain, KeyValueEntity>
     implements KeyValueRepo {
-  late KeyValueRepoExternal _external; //TODO: remove
-
   KeyValueRepoImpl(
-      KeyValueRepoExternal repo, KeyValueSingleConverter kvConverter)
-      : super(externalRepo: repo, converter: KeyValueConverter(kvConverter)) {
-    _external = repo;
-  }
+      KeyValueRepoExternal repo, SingleKeyValueConverter kvConverter)
+      : super(externalRepo: repo, converter: KeyValueConverter.converter);
 }
 
 class KeyValueConverter
     extends DefaultGeneralConverter<KeyValueDomain, KeyValueEntity> {
-  KeyValueSingleConverter kvConverter;
+  static final KeyValueConverter converter = KeyValueConverter._();
 
-  KeyValueConverter(this.kvConverter);
+  KeyValueConverter._();
 
   @override
   KeyValueDomain toDomain(KeyValueEntity entity) {
     return KeyValueDomain(
       id: entity.id,
-      key: kvConverter.stringToKey(entity.key),
-      value: kvConverter.stringToValue(entity.value),
+      key: entity.key,
+      value: entity.value,
     );
   }
 
   @override
   KeyValueEntity toEntity(KeyValueDomain domain) {
     return KeyValueEntity(
-      kvConverter.keyToString(domain.key),
-      kvConverter.valueToString(domain.value),
+      domain.key,
+      domain.value,
       id: domain.id,
     );
   }
