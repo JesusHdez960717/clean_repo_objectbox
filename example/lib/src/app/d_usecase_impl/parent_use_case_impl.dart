51bc03e27eecaa65e7753295480f3d5c67e01d5e
JHB <jhernandezb96@gmail.com>
2022-04-07 22:44:58 -0400

    Migrated code to clean 1.3.3

--- example/lib/src/app/d_usecase_impl/parent_use_case_impl.dart
+++ example/lib/src/app/d_usecase_impl/parent_use_case_impl.dart
@@ -3,15 +3,11 @@ import 'dart:async';
 import 'package:clean_core/clean_core.dart';
 import 'package:clean_repo_objectbox_example/src/app/objectbox_app_exporter.dart';
 
-class ParentUseCaseImpl extends DefaultCRUDUseCase<ParentDomain>
+class ParentUseCaseImpl extends DefaultCRUDUseCase<ParentDomain, ParentRepo>
     implements ParentUseCase {
-  late ParentRepo _repo;
-
-  ParentUseCaseImpl(ParentRepo repo)
-      : _repo = repo,
-        super(repo: repo);
+  ParentUseCaseImpl(ParentRepo repo) : super(repo: repo);
 
   StreamController<List<ParentDomain>> streamController() {
-    return _repo.streamController();
+    return repo.streamController();
   }
 }
--- lib/src/repo/objectbox/key_value_repo/app/d_usecase_impl/key_value_use_case_impl.dart
+++ lib/src/repo/objectbox/key_value_repo/app/d_usecase_impl/key_value_use_case_impl.dart
@@ -1,11 +1,8 @@
 import 'package:clean_core/clean_core.dart';
 import 'package:clean_repo_objectbox/src/repo/objectbox/key_value_repo/app/key_value_app_exporter.dart';
 
-class KeyValueUseCaseImpl extends DefaultCRUDUseCase<KeyValueDomain>
+class KeyValueUseCaseImpl
+    extends DefaultCRUDUseCase<KeyValueDomain, KeyValueRepo>
     implements KeyValueUseCase {
-  late KeyValueRepo _repo;
-
-  KeyValueUseCaseImpl(KeyValueRepo repo)
-      : _repo = repo,
-        super(repo: repo);
+  KeyValueUseCaseImpl(KeyValueRepo repo) : super(repo: repo);
 }
